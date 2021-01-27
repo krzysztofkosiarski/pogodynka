cd308d29867b8010579ad615fae78c1311bff8e1
krzysztofkosiarski <krzysztof.kosiarski@interia.pl>
2021-01-27 13:09:39 +0100

    zmiana komentarzy

--- pogodynka.py
+++ pogodynka.py
@@ -1,32 +1,34 @@
-#import potrzebnych bibliotek
+# import potrzebnych bibliotek
 import tkinter as tk
 from requests import get
 from json import loads
 
-#tworzenie okna aplikacji
+# tworzenie okna aplikacji
 frame = tk.Tk()
-frame.title('Pomiar temperatury - Źródłem pochodzenia danych jest Instytut Meteorologii i Gospodarki Wodnej – Państwowy Instytut Badawczy')
+frame.title(
+    'Pomiar temperatury - Źródłem pochodzenia danych jest Instytut Meteorologii i Gospodarki Wodnej – Państwowy Instytut Badawczy')
 
-#tworzenie pola textowego z tytułem
+# tworzenie pola textowego z tytułem
 label = tk.Label(frame, text='Wpisz miasto', width=20)
 label.pack()
 
-#tworzenie pola na wpisanie miasta
+# tworzenie pola na wpisanie miasta
 entry = tk.Entry(0, width=25)
 entry.pack()
 
-#tworzenie pola wyniku pomiaru
+# tworzenie pola wyniku pomiaru
 text = tk.Text(0, width=70, height=5)
 text.pack()
 
-#funkcja pobierająca dane z imgw
+
+# funkcja pobierająca dane z imgw
 def main():
     url = 'https://danepubliczne.imgw.pl/api/data/synop'
     response = get(url)
     city = entry.get()
     city = city.capitalize()
     rows = [
-        ['Miasto', 'Godzina pomiaru', 'Temperatura'] #selekcja informacji z api
+        ['Miasto', 'Godzina pomiaru', 'Temperatura']  # selekcja informacji z api
     ]
     for row in loads(response.text):
         if row['stacja'] in city:
@@ -36,16 +38,18 @@ def main():
             q = (f'Temperatura w mieście {a} o godzinie {c} wynosiła {b} \n')
             text.insert('1.0', q)
 
-#funkcja czyszcząca pola na wynik pomiaru
+
+# funkcja czyszcząca okno miasta i pomiaru temperatury
 def delete_text():
     text.delete('1.0', tk.END)
     entry.delete('0', tk.END)
 
-#tworzenie przycisku sprawdzenia
+
+# tworzenie przycisku sprawdzenia
 button = tk.Button(frame, text='Sprawdź', padx=50, command=main)
 button.pack()
 
-#tworzenie przycisku czyszczenia okna miasta i pomiaru
+# tworzenie przycisku czyszczenia okna miasta i pomiaru temperatury
 button_2 = tk.Button(frame, text='Wyczyść', padx=50, command=delete_text)
 button_2.pack()
 
